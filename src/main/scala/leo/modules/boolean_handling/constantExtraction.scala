57c02d57c218bc0e1d370219f7b3b1b3eaad64b6
Hans-Jörg Schurr <commits@schurr.at>
2016-07-11 19:43:31 +0200

    progress: pure literal detection

--- src/main/scala/leo/modules/boolean_handling/constantExtraction.scala
+++ src/main/scala/leo/modules/boolean_handling/constantExtraction.scala
@@ -7,13 +7,14 @@ import leo.modules.calculus.mayUnify
 import scala.collection.SortedSet
 
 /**
-  * Created by hanse on 6/13/16.
+  * Created by Hans-Jörg Schurr on 6/13/16.
   */
 object constantExtraction {
 
   /* TODO:
     - Not unifiable test
     - Search (double iteration!)
+      - is pure: doesn't unify with any literal of opposite polarity
     - return list of pure literals (for now)
    */
   def pureLiterals(clauses : SortedSet[AnnotatedClause]) : Set[AnnotatedClause] = {
@@ -27,11 +28,28 @@ object constantExtraction {
 
     val testSet : List[List[(Literal, Boolean)]] = clauses.map((_).cl.map(l => (l, true)).toList).toList
 
-    /*
-    def test(l) = {
-      case (l::)
-    }*/
+    type LiteralP = (Literal, Boolean)
+    type Matrix = List[List[LiteralP]]
 
+    def testClause(c1: List[LiteralP], c2: List[LiteralP]) : (List[LiteralP], List[LiteralP]) =
+      (c1, c2)
+
+    def search(accu : (List[LiteralP], Matrix), c2: List[LiteralP]) =
+      accu match {
+        case (c1, accuM) =>
+          val (nC1, nC2) = testClause(c1, c2)
+          (nC1, nC2::accuM)
+      }
+
+    def test(matrix : Matrix) : Matrix =
+      matrix match {
+        case c :: cs =>
+          val(resC, newMatrix) = cs.foldLeft((c, cs))(search)
+          resC::test(newMatrix)
+        case _  => List()
+    }
+
+    test(testSet)
     Out.debug(s"### Pure literal dedection.")
     for (c <- clauses) {
       Out.debug(s"## Got: ${c.pretty}")

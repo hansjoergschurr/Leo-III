ccd59732dbc45dcfbc315cfc67814daf27607bcc
Hans-Jörg Schurr <commits@schurr.at>
2016-07-12 18:55:31 +0200

    pure literal detection: untested implementation

--- src/main/scala/leo/modules/boolean_handling/constantExtraction.scala
+++ src/main/scala/leo/modules/boolean_handling/constantExtraction.scala
@@ -18,21 +18,34 @@ object constantExtraction {
     - return list of pure literals (for now)
    */
   def pureLiterals(clauses : SortedSet[AnnotatedClause]) : Set[AnnotatedClause] = {
-
-    /** Tests unifiability approximately. True indicates, that the terms are certainly not unifiable, False
-      * indicates that they might be unifiable.
-      */
-    def notUnifiable(t1: Term, t2: Term): Boolean = {
-      !mayUnify(t1, t2)
-    }
-
-    val testSet : List[List[(Literal, Boolean)]] = clauses.map((_).cl.map(l => (l, true)).toList).toList
+    val testSet : List[List[(Literal, Boolean)]] = clauses.map((_).cl.map(l => (l, !l.equational)).toList).toList
 
     type LiteralP = (Literal, Boolean)
     type Matrix = List[List[LiteralP]]
 
-    def testClause(c1: List[LiteralP], c2: List[LiteralP]) : (List[LiteralP], List[LiteralP]) =
-      (c1, c2)
+    def testClause(c1: List[LiteralP], c2: List[LiteralP]) : (List[LiteralP], List[LiteralP]) = {
+      val ch :: cs = c1
+
+      def scanner(t: (LiteralP, List[LiteralP]), lit: LiteralP): (LiteralP, List[LiteralP]) = t match {
+        case (_, c2) =>
+          var (l1,p1) = lit
+          val c2n = for((l2,p2) <- c2) yield {
+            // TODO: Extend to equational literals
+            if (l1.polarity != l2.polarity &&
+              !l1.equational && !l2.equational
+              && mayUnify(l1.left, l2.left)) {
+              p1 = false
+              (l2, false)
+            }
+            else
+              (l2,p2)
+          }
+          ((l1,p1), c2n)
+      }
+
+      val s = cs.scanLeft((ch,c2))(scanner)
+      (s.map(_._1), s.last._2)
+    }
 
     def search(accu : (List[LiteralP], Matrix), c2: List[LiteralP]) =
       accu match {
@@ -49,11 +62,16 @@ object constantExtraction {
         case _  => List()
     }
 
-    test(testSet)
     Out.debug(s"### Pure literal dedection.")
+
+    val pur = test(testSet)
+
     for (c <- clauses) {
       Out.debug(s"## Got: ${c.pretty}")
     }
+    for(c <- pur; (l, p) <- c; if p)
+      Out.debug(s"## Pure: ${l.pretty}")
+
     Set.empty
   }
 }

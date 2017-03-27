7f9e5ef380901283e1705d4a8d2c073faa52f06d
Hans-Jörg Schurr <commits@schurr.at>
2017-03-27 16:24:02 +0200

    WIP blocked clause elimination. Main skeleton done, validity check missing.

--- src/main/scala/leo/modules/special_processing/BlockedClauseElimination.scala
+++ src/main/scala/leo/modules/special_processing/BlockedClauseElimination.scala
@@ -3,8 +3,9 @@ package leo.modules.special_processing
 import leo.datastructures.Term.Bound
 import leo.datastructures._
 import leo.modules.HOLSignature.===
-import leo.modules.calculus.{CalculusRule, PatternUnification, mayUnify}
+import leo.modules.calculus.{CalculusRule, PatternUnification}
 import leo.modules.output.SZS_EquiSatisfiable
+import leo.modules.output.logger.Out
 
 import scala.collection.mutable
 import scala.collection.mutable.{HashMap, MultiMap, Set}
@@ -58,6 +59,44 @@ class PatternIndex(implicit sig: Signature) {
   { lookupResCandidates(l).flatMap(t => if (t._1 == c) Some(t._2) else None)}
 
   def numTotalPartners(l: Literal) : Int = {lookupResCandidates(l).size}
+
+  def removeClause(c: Clause) : Unit = {
+    flexNegative.retain(_._1 != c)
+    flexPositive.retain(_._1 != c)
+    rigidPositive.retain(_._1 != c)
+    rigidNegative.retain(_._1 != c)
+    rigidIndex.mapValues(s => {s.retain(_._1 != c); s})
+  }
+
+  def debugOutput() : Unit = {
+    Out.debug(s"Pattern stats:")
+    Out.debug(s"\tFlex Positive: ${flexPositive.size}")
+    Out.debug(s"\tFlex Negative: ${flexNegative.size}")
+    Out.debug(s"\tRigid Positive: ${rigidPositive.size}")
+    Out.debug(s"\tRigid Negative: ${rigidNegative.size}")
+    Out.debug(s"\tRigid Index Keys: ${rigidIndex.keys.size}")
+  }
+}
+
+class Deactivations(implicit sig: Signature) {
+  private val deactivatedBy = new HashMap[(Clause, Literal), Set[Clause]] with MultiMap[(Clause, Literal), Clause]
+  private val deactivates =  new HashMap[Clause, Set[(Clause, Literal)]] with MultiMap[Clause, (Clause, Literal)]
+
+  def deactivates(deactivator: Clause, deactivated: (Clause, Literal)) {
+    deactivatedBy.addBinding(deactivated, deactivator)
+    deactivates.addBinding(deactivator, deactivated)
+  }
+
+  def isDeactivated(deactivated: (Clause, Literal)): Boolean = deactivatedBy(deactivated).nonEmpty
+
+  def removeDeactivator(deactivator: Clause): Set[(Clause, Literal)] = {
+    val d = deactivates(deactivator)
+    deactivates.remove(deactivator)
+    d.foreach(cl => deactivatedBy.removeBinding(cl, deactivator))
+    d.filter(cl => deactivatedBy(cl).isEmpty)
+  }
+
+  def numDeactivated : Int = deactivatedBy.keys.size
 }
 
 /**
@@ -72,7 +111,7 @@ object BlockedClauseElimination extends CalculusRule {
     * @param clauses Set of clauses.
     * @return True if the input set is equality free.
     */
-  def isEqualityFree(clauses: Set[AnnotatedClause])(implicit  sig: Signature) : Boolean = {
+  def isEqualityFree(clauses: scala.collection.immutable.Set[AnnotatedClause])(implicit  sig: Signature) : Boolean = {
     clauses.forall(_.cl.lits.forall( l => {
       ! (l.equational ||
         l.left.symbols.contains(===.key) ||
@@ -80,20 +119,16 @@ object BlockedClauseElimination extends CalculusRule {
     }))
   }
 
-  def isNotResOrValid(C: Clause, D: Clause, blockingLit: Term, resLit: Term)(implicit sig: Signature) : Boolean = {
-    if(mayUnify(blockingLit, resLit)) {
-      if (!PatternUnification.isPattern(resLit)) false
-      else {
-        val maxFree = C.maxImplicitlyBound
-        val D_lifted = D.substitute(Subst.shift(maxFree))
-        val resLit_lifted = resLit.substitute(Subst.shift(maxFree))
-        val resolvent = Clause(C.lits ++ D.lits)
-        val vargen = leo.modules.calculus.freshVarGen(resolvent)
-        val uni = PatternUnification.unify(vargen, blockingLit, resLit_lifted)
-        false
-      }
-    }
-    else true
+  def isNotResOrValid(C: Clause, blockingLit: Literal, D: Clause, resLit: Literal)(implicit sig: Signature) : Boolean = {
+    val resolvent = Clause(C.lits ++ D.lits)
+    val vargen = leo.modules.calculus.freshVarGen(resolvent)
+    val maxFree = C.maxImplicitlyBound
+
+    val D_lifted = D.substitute(Subst.shift(maxFree))
+    val resLit_lifted = resLit.substitute(Subst.shift(maxFree))
+
+    val uni = PatternUnification.unify(vargen, blockingLit.left, resLit_lifted.left)
+    true
   }
 
   /**
@@ -103,7 +138,7 @@ object BlockedClauseElimination extends CalculusRule {
     * @param clauses the matrix. Must be equality free
     * @return new set of clauses
     */
-  def removeBlockedClauses(clauses : Set[AnnotatedClause])(implicit sig: Signature) : Set[AnnotatedClause] = {
+  def removeBlockedClauses(clauses : scala.collection.immutable.Set[AnnotatedClause])(implicit sig: Signature) : scala.collection.immutable.Set[AnnotatedClause] = {
     assert(isEqualityFree(clauses))
 
     // Iterate once and separate
@@ -112,24 +147,30 @@ object BlockedClauseElimination extends CalculusRule {
     //  -> Non-pattern with bound head: Create mapping (Head, Polarity) -> (C, L)
     //    -> Deactivate (C, L) pattern pairs that have a head in the non-pattern mapping with foreign clause
 
-
     val rigidPatternIndex = new PatternIndex
     val rigidNonPatterns = new HashMap[(Boolean, Term), Set[(Clause, Literal)]] with MultiMap[(Boolean, Term), (Clause, Literal)]
-    var deactivatedPairs = List[(Clause, Literal)]()
 
+    var clauseLiteralPairs = List[(Clause, Literal)]()
+    var clauseContainingFlexNP : Option[Clause] = None
+    // Find all clause literal pairs and add literals to indices
     clauses.foreach(ac => {
       val c = ac.cl
       c.lits.foreach(l => {
         if(PatternUnification.isPattern(l.left)) {
           rigidPatternIndex.addClauseLiteralPair(c,l)
-          deactivatedPairs = (c, l)::deactivatedPairs
+          clauseLiteralPairs = (c, l)::clauseLiteralPairs
         }
-        else {
+        else { // non-pattern case
           l.left.headSymbol match {
-          // Flex case
+          // Flex case (evil one)
           case Bound(_, _) =>
-            // here be dragons!
-            ()
+            clauseContainingFlexNP match {
+              case None => clauseContainingFlexNP = Some(c)
+              case Some(c2) => if (c != c2) {
+                Out.debug{"Found two different clauses with non-pattern literals with flex head. Aborting Blocked Clause Elimination."}
+                return clauses
+              }
+            }
           // Rigid case
           case head =>
             rigidNonPatterns.addBinding((l.polarity, head), (c, l))
@@ -138,17 +179,65 @@ object BlockedClauseElimination extends CalculusRule {
       })
     })
 
-    val queue = mutable.PriorityQueue
-    var foundBlocked = Set.empty[AnnotatedClause]
+    // Add clause literal pairs to queue and deactivate appropriately
+    val deactivations = new Deactivations()
+    val queue = new mutable.PriorityQueue[((Clause, Literal), Int)]()(Ordering.by(_._2))
 
-    //TODO: optimize a bit
-    for (possibleBlocked <- clauses) {
-      for (possibleBlockingLit <- possibleBlocked.cl.lits.map(_.left).withFilter(PatternUnification.isPattern)) {
-        val isBlocked = clauses.forall(c => c == possibleBlocked || c.cl.lits.forall(l =>
-          isNotResOrValid(possibleBlocked.cl, c.cl, possibleBlockingLit, l.left)))
-        if(isBlocked) foundBlocked = foundBlocked + possibleBlocked
+    clauseContainingFlexNP match {
+      case Some(c:Clause) => {
+        // Deactivate all pairs except the ones induced by c
+        Out.debug(s"Found one clause containing non-pattern with flex head. Deactivating all other clauses.")
+        c.lits.foreach{l =>
+          if (PatternUnification.isPattern(l.left)) {
+            queue.enqueue((c,l) -> rigidPatternIndex.numTotalPartners(l))
+            clauseLiteralPairs.foreach(cl => {
+              deactivations.deactivates(c, cl)
+            })
+          }
+        }
+      }
+      case None => {
+        // Either add to queue, or deactivate if rigid non patterns with same head and opposite polarity are found.
+        Out.debug(s"Found no clause containing non-pattern with flex head.")
+        clauseLiteralPairs.foreach((cl: (Clause, Literal)) => {
+          val rnp = rigidNonPatterns((! cl._2.polarity, cl._2.left))
+          if(rnp.isEmpty) {
+            queue.enqueue(cl -> rigidPatternIndex.numTotalPartners(cl._2))
+          }
+          else {
+            rnp.foreach(d_cl => deactivations.deactivates(d_cl._1, cl))
+          }
+        })
       }
     }
-    clauses
+
+    rigidPatternIndex.debugOutput()
+    Out.debug(s"Deactivated pairs: ${deactivations.numDeactivated}.")
+
+    val foundBlocked = Set.empty[Clause]
+
+    while (queue.nonEmpty) {
+      val ((c,l), _) = queue.dequeue()
+      val s = rigidPatternIndex.lookupResCandidates(l)
+      val blocked = s.forall(cl => {
+        val c2 = cl._1
+        val l2 = cl._2
+        val b = isNotResOrValid(c, l, c2, l2)
+        if(b) {
+          deactivations.deactivates(c2, (c,l))
+          false
+        }
+        else true
+      })
+      if (blocked) {
+        foundBlocked.add(c)
+        rigidPatternIndex.removeClause(c)
+        val reactivated = deactivations.removeDeactivator(c)
+        reactivated.foreach(cl => queue.enqueue((cl._1, cl._2) -> rigidPatternIndex.numTotalPartners(cl._2)))
+      }
+    }
+
+    Out.debug(s"Found blocked: ${foundBlocked.size}")
+    clauses.filter(ac => !foundBlocked.contains(ac.cl))
   }
 }
